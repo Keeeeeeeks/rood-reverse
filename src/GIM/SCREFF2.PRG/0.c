27e8e8b25e29b1d3ff0c71053cba60315dd44f06
Keeeeeeeks <gof3@cornell.edu>
2026-05-10 21:22:10 -0400

    Draft SCREFF2 frontier
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- src/GIM/SCREFF2.PRG/0.c
+++ src/GIM/SCREFF2.PRG/0.c
@@ -5,8 +5,10 @@
 
 void func_8007DFF0(int, int, int);
 void func_8007E0A8(int, int, int);
+u_long* func_800C0230(int, int, int, u_long*);
 
 extern int* D_1F800000[];
+extern int vs_main_frameBuf;
 
 void func_800F9800(int arg0)
 {
@@ -22,7 +24,104 @@ void func_800F9800(int arg0)
     }
 }
 
+// Milestone 3 first target: display-list setup; generated asm is non-handwritten.
+#if defined(PERMUTER) || defined(OBJDIFF)
+void func_800F986C(void)
+{
+    u_long* prim;
+    u_long* ot;
+    u_long** scratch;
+    int outer;
+    int y;
+    int x;
+    int part;
+    int tex;
+    int xy;
+    int fbX;
+    int offsetA;
+    int offsetB;
+
+    scratch = (u_long**)0x1F800000;
+    ot = scratch[1];
+
+    offsetA = 0xDE0000;
+    offsetB = 0xC20000;
+    for (outer = 0xC0; outer >= 0; outer -= 0x20) {
+        for (y = 0x100; y >= 0; y -= 0x40) {
+            tex = ((((y + 0x2C0) & 0x3FF) >> 6) | 0x170);
+            for (x = 0x40; x >= 0; x -= 0x20) {
+                if (x == 0x40) {
+                    prim = func_800C0230(0x160, (y + 2) | offsetA, 0x40040, ot);
+                    xy = (outer + 0x1C) << 8;
+                } else {
+                    prim = func_800C0230(0x160, (y + x + 0x1E) | offsetB, 0x1C0004, ot);
+                    xy = (x + 0x1C) | (outer << 8);
+                }
+                prim[1] = 0xE1000200 | tex;
+                prim[4] = xy;
+            }
+        }
+        offsetA += 0xFFE00000;
+        offsetB += 0xFFE00000;
+    }
+
+    offsetB = 0xC20000;
+    for (outer = 0xC0; outer >= 0; outer -= 0x20) {
+        for (y = 0x120; y >= 0; y -= 0x20) {
+            for (part = 0; part < 3; ++part) {
+                if (part == 0) {
+                    prim = func_800C0230(0x160, (y + 2) | offsetB, 0x1C001C, ot);
+                } else if (part == 1) {
+                    prim =
+                        func_800C0230(0x160, (y - 2) | ((outer - 2) << 16), 0x200020, ot);
+                } else {
+                    prim = func_800C0230(0x60, y | (outer << 16), 0x200020, ot);
+                }
+
+                tex = ((((y & 0x1C0) + 0x2C0) & 0x3FF) >> 6);
+                if (part == 0) {
+                    tex |= 0x170;
+                } else {
+                    tex |= 0x110;
+                }
+                prim[1] = 0xE1000200 | tex;
+                prim[4] = (y & 0x20) | (outer << 8);
+            }
+        }
+        offsetB += 0xFFE00000;
+    }
+
+    prim = scratch[0];
+
+    x = D_800EB9B4->unk2 - 3;
+    prim[0] = (ot[x] & 0xFFFFFF) | 0x07000000;
+    prim[1] = 0xE30402C0;
+    prim[2] = 0xE4077FFF;
+    prim[3] = 0xE50802C0;
+    prim[4] = 0x60000000;
+    prim[5] = 0;
+    prim[6] = 0xE00140;
+    prim[7] = 0xE6000001;
+    ot[x] = ((u_long)prim << 8) >> 8;
+    prim += 8;
+
+    fbX = 0;
+    if (vs_main_frameBuf == 0) {
+        fbX = 0x140;
+    }
+
+    prim[0] = (ot[0] & 0xFFFFFF) | 0x04000000;
+    prim[1] = 0xE3000000 | fbX;
+    prim[2] = 0xE4037C00 | (fbX + 0x13F);
+    prim[3] = 0xE5000000 | fbX;
+    prim[4] = 0xE6000000;
+    ot[0] = ((u_long)prim << 8) >> 8;
+
+    ((u_long**)0x1F800000)[0] = prim + 5;
+}
+#else
 INCLUDE_ASM("build/src/GIM/SCREFF2.PRG/nonmatchings/0", func_800F986C);
+#endif
 
 void func_800F9BC0(short arg0, short arg1)
 {
@@ -30,6 +129,7 @@ void func_800F9BC0(short arg0, short arg1)
     D_800EB9B4->unk6 = arg1;
 }
 
+// Generated asm marks this as handwritten and uses raw GTE instructions.
 INCLUDE_ASM("build/src/GIM/SCREFF2.PRG/nonmatchings/0", func_800F9BD8);
 
 void func_800F9DE8(int arg0) { D_800EB9B4->unk8 = arg0; }
